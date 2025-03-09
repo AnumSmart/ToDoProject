8a4c3a2c1f08c6cf26b7744dbb510a2216c56d69
Aliaksei Makarevich <electron2686@gmail.com>
2025-03-09 15:57:08 +0300

    add handlers and routes

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 	todo "todoproject"
+	"todoproject/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run("8080"); err != nil {
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occurred while running http server: %s\n", err.Error())
 	}
 
